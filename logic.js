9aa4d670394217b65280a3d00e32ac40505bfe39
AbdulMoiz8994 <moiza8994@gmail.com>
2021-10-13 22:18:26 +0500

    ad the initai login

---
+++ logic.js
@@ -0,0 +1,9 @@
+let form=document.querySelector('.form')
+let username=document.querySelector('#username')
+let email=document.querySelector('#email')
+let password=document.querySelector('#password')
+let confirmPassword=document.querySelector('#confirmpassword')
+
+
+
+form.addEventListener()
